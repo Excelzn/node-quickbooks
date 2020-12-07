93766d40dfa771c8d28673b44ac8b0949de97ed0
Bill Sowle <bill@timelabs.com>
2020-12-07 11:57:55 -0700

    Adding tests for typescript version

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import _ from 'underscore';
 import request from 'request';
+import * as pjson from '../package.json';
+import * as uuid from 'uuid';
+import jxon from 'jxon';
+import util from 'util';
+import debug from 'request-debug';
+import axios from 'axios';
 
 export interface QuickbooksConfiguration {
     consumerKey: string;
@@ -18,13 +24,13 @@ export interface QuickbooksConfiguration {
 
 
 export class Quickbooks {
-    private consumerKey: string;
-    private consumerSecret: string;
-    private token: string;
+    private consumerKey: string|null;
+    private consumerSecret: string|null;
+    private token: string|null;
     private tokenSecret: string|null;
-    private realmId: string;
-    private useSandbox: boolean;
-    private debug: boolean;
+    private realmId: string|null;
+    private useSandbox: boolean|null;
+    private debug: boolean|null;
     private endpoint: string;
     private minorversion: string;
     private oauthversion: string;
@@ -35,6 +41,12 @@ export class Quickbooks {
     public static readonly QUERY_OPERATORS = ['=', 'IN', '<', '>', '<=', '>=', 'LIKE'];
     public static readonly TOKEN_URL = 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer';
     public static readonly REVOKE_URL = 'https://developer.api.intuit.com/v2/oauth2/tokens/revoke';
+    public static REQUEST_TOKEN_URL: string|null;
+    public static ACCESS_TOKEN_URL: string|null;
+    public static APP_CENTER_URL: string|null;
+    public static RECONNECT_URL: string|null;
+    public static DISCONNECT_URL: string|null;
+    public static USER_INFO_URL: string|null;
     public static readonly OAUTH_ENDPOINTS = {
         '1.0a': function (callback) {
             callback({
@@ -81,15 +93,15 @@ export class Quickbooks {
     };
 
 
-    constructor(consumerKey: string|QuickbooksConfiguration, consumerSecret: string, token: string, tokenSecret: string|null, realmId: string, useSandbox: boolean, debug: boolean, minorversion: string, oauthversion: string, refreshToken: string|null) {
+    constructor(consumerKey: string|QuickbooksConfiguration, consumerSecret?: string, token?: string, tokenSecret?: string|null, realmId?: string, useSandbox?: boolean, debug?: boolean, minorversion?: string, oauthversion?: string, refreshToken?: string|null) {
         if(typeof consumerKey === 'string') {
             this.consumerKey = consumerKey;
-            this.consumerSecret = consumerSecret;
-            this.token = token;
-            this.tokenSecret = tokenSecret;
-            this.realmId = realmId;
-            this.useSandbox = useSandbox;
-            this.debug = debug;
+            this.consumerSecret = consumerSecret ?? null;
+            this.token = token ?? null;
+            this.tokenSecret = tokenSecret ?? null;
+            this.realmId = realmId ?? null;
+            this.useSandbox = useSandbox ?? null;
+            this.debug = debug ?? null;
             this.minorversion = minorversion || '4';
             this.oauthversion = oauthversion || '1.0a';
             this.refreshToken = refreshToken || null;
@@ -109,6 +121,257 @@ export class Quickbooks {
 
         if(!this.tokenSecret == null && this.oauthversion !== '2.0') {
             throw new Error('tokenSecret not defined');
+        }
+    }
+
+    public setOauthVersion(version: string|number, useSandbox: boolean): void {
+        version = (typeof version === 'number') ? version.toFixed(1): version;
+        const discoveryUrl = useSandbox ? 'https://developer.intuit.com/.well-known/openid_sandbox_configuration/' : 'https://developer.api.intuit.com/.well-known/openid_configuration/';
+        Quickbooks.OAUTH_ENDPOINTS[version](function (endpoints: { [x: string]: any; }) {
+            for (let k in endpoints) {
+                Quickbooks[k] = endpoints[k];
+            }
+        }, discoveryUrl);
+    }
+
+    public refreshAccessToken(callback) {
+        const auth = (new Buffer(this.consumerKey + ':' + this.consumerSecret).toString('base64'));
+        const postBody = {
+            url: Quickbooks.TOKEN_URL,
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                Authorization: 'Basic ' + auth,
+            },
+            form: {
+                grant_type: 'refresh_token',
+                refresh_token: this.refreshToken
+            }
+        };
+        request.post(postBody, ((e, r, data) => {
+            if (r && r.body) {
+                const refreshResponse = JSON.parse(r.body);
+                this.refreshToken = refreshResponse.refresh_token;
+                this.token = refreshResponse.access_token;
+                if (callback) callback(e, refreshResponse);
+            } else {
+                if (callback) callback(e, r, data);
+            }
+        }));
+    }
+
+    public revokeAccess(useRefresh: boolean, callback) {
+        const auth = (new Buffer(this.consumerKey + ':' + this.consumerSecret).toString('base64'));
+        const revokeToken = useRefresh ? this.refreshToken : this.token;
+        const postBody = {
+            url: Quickbooks.REVOKE_URL,
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                Authorization: 'Basic ' + auth,
+            },
+            form: {
+                token: revokeToken
+            }
+        };
+        request.post(postBody, ((e, r, data) => {
+            if (r && r.statusCode === 200) {
+                this.refreshToken = null;
+                this.token = null;
+                this.realmId = null;
+            }
+            if (callback) callback(e, r, data);
+        }));
+    }
+    public getUserInfo(callback) {
+        this.request( 'get', {url: Quickbooks.USER_INFO_URL}, null, callback);
+    }
+
+    public batch(items: Object[], callback) {
+        this.request( 'post', {url: '/batch'}, {BatchItemRequest: items}, callback)
+    }
+    public reconnect(callback) {
+        this.xmlRequest( Quickbooks.RECONNECT_URL ?? '', 'ReconnectResponse', callback);
+    }
+    public disconnect(callback) {
+        this.xmlRequest( Quickbooks.DISCONNECT_URL ?? '', 'PlatformResponse', callback);
+    }
+
+    //CRUD endpoints
+    //Invoice
+    public createInvoice (invoice, callback) {
+        this.create( 'invoice', invoice, callback);
+    }
+
+
+    private request(verb: string, options: any, entity: any, callback) {
+        const version = pjson.version;
+        let url = this.endpoint + this.realmId + options.url;
+        if (options.url === Quickbooks.RECONNECT_URL || options.url == Quickbooks.DISCONNECT_URL || options.url === Quickbooks.REVOKE_URL || options.url === Quickbooks.USER_INFO_URL) {
+            url = options.url
+        }
+        const opts: any = {
+            url:     url,
+            qs:      options.qs || {},
+            headers: options.headers || {},
+            json:    true
+        }
+        //build query string
+        if (entity && entity.allowDuplicateDocNum) {
+            delete entity.allowDuplicateDocNum;
+            opts.qs.include = 'allowduplicatedocnum';
+        }
+
+        if (entity && entity.requestId) {
+            opts.qs.requestid = entity.requestId;
+            // noinspection JSConstantReassignment
+            delete entity.requestId;
+        }
+
+        opts.qs.minorversion = opts.qs.minorversion || this.minorversion;
+        opts.qs.format = 'json';
+        //build headers
+        opts.headers['User-Agent'] = 'node-quickbooks: version ' + version
+        opts.headers['Request-Id'] = uuid.v1()
+        if (this.oauthversion == '2.0'){
+            opts.headers['Authorization'] =  'Bearer ' + this.token
+        } else {
+            opts.oauth = this.oauth();
+        }
+        if (options.url.match(/pdf$/)) {
+            opts.headers['accept'] = 'application/pdf'
+            opts.encoding = null
+        }
+        if (entity !== null) {
+            opts.body = entity
+        }
+        if (options.formData) {
+            opts.formData = options.formData
+        }
+        if ('production' !== process.env.NODE_ENV && this.debug) {
+            debug(request)
+        }
+        request[verb].call(this, opts, (err, res, body) => {
+            if ('production' !== process.env.NODE_ENV && this.debug) {
+                console.log('invoking endpoint: ' + url)
+                console.log(entity || '')
+                console.log(JSON.stringify(body, null, 2));
+            }
+            if (callback) {
+                if (err ||
+                    res.statusCode >= 300 ||
+                    (_.isObject(body) && body.Fault && body.Fault.Error && body.Fault.Error.length) ||
+                    (_.isString(body) && !_.isEmpty(body) && body.indexOf('<') === 0)) {
+                    callback(err || body, body, res)
+                } else {
+                    callback(null, body, res)
+                }
+            }
+        })
+    }
+
+    private xmlRequest(url: string, rootTag: string, callback) {
+        this.request( 'get', {url:url}, null, (err, body) => {
+            const json =
+                body.constructor === {}.constructor ? body :
+                    (body.constructor === "".constructor ?
+                        (body.indexOf('<') === 0 ? jxon.stringToJs(body)[rootTag] : body) : body);
+            callback(json.ErrorCode === 0 ? null : json, json);
+        })
+    }
+    private oauth() {
+        return {
+            consumer_key:    this.consumerKey,
+            consumer_secret: this.consumerSecret,
+            token:           this.token,
+            token_secret:    this.tokenSecret
+        }
+    }
+
+    private unwrap(callback, entityName) {
+        if (! callback) return (err, data) => {}
+        return (err, data) => {
+            if (err) {
+                if (callback) callback(err)
+            } else {
+                const name = this.capitalize(entityName)
+                if (callback) callback(err, (data || {})[name] || data)
+            }
+        }
+    }
+    private capitalize(s: string): string {
+        return s.substring(0, 1).toUpperCase() + s.substring(1)
+    }
+    private pluralize(callback, entityName) {
+        if (! callback) return function(err, data) {}
+        return (err, data) => {
+            if (err) {
+                if (callback) callback(err)
+            } else {
+                const name = this.capitalize(entityName)
+                if (callback) callback(err, (data || {})[name] || data)
+            }
+        }
+    }
+
+    private create(entityName: string, entity: any, callback) {
+        const url = '/' + entityName.toLowerCase()
+        this.request( 'post', {url: url}, entity, this.unwrap(callback, entityName))
+    }
+    private read(entityName: string, id: string, callback) {
+        let url = '/' + entityName.toLowerCase()
+        if (id) url = url + '/' + id
+        this.request( 'get', {url: url}, null, this.unwrap(callback, entityName))
+    }
+    private update(entityName: string, entity: any, callback) {
+        if (_.isUndefined(entity.Id) ||
+            _.isEmpty(entity.Id + '') ||
+            _.isUndefined(entity.SyncToken) ||
+            _.isEmpty(entity.SyncToken + '')) {
+            if (entityName !== 'exchangerate') {
+                throw new Error(entityName + ' must contain Id and SyncToken fields: ' +
+                    util.inspect(entity, {showHidden: false, depth: null}))
+            }
+        }
+        if (! entity.hasOwnProperty('sparse')) {
+            entity.sparse = true
+        }
+        const url = '/' + entityName.toLowerCase() + '?operation=update'
+        const opts: any = {url: url}
+        if (entity.void && entity.void.toString() === 'true') {
+            opts.qs = { include: 'void' }
+            delete entity.void
+        }
+        this.request( 'post', opts, entity, this.unwrap(callback, entityName))
+    }
+    private delete(entityName: string, idOrEntity: string|any, callback) {
+        const url = '/' + entityName.toLowerCase() + '?operation=delete'
+        callback = callback || function() {}
+        if (_.isObject(idOrEntity)) {
+            this.request( 'post', {url: url}, idOrEntity, callback)
+        } else {
+            this.read( entityName, idOrEntity, (err, entity) => {
+                if (err) {
+                    callback(err)
+                } else {
+                    this.request('post', {url: url}, entity, callback)
+                }
+            })
+        }
+    }
+    private void(entityName: string, idOrEntity: string|any, callback) {
+        const url = '/' + entityName.toLowerCase() + '?operation=void'
+        callback = callback || function () { }
+        if (_.isObject(idOrEntity)) {
+            this.request( 'post', { url: url }, idOrEntity, callback)
+        } else {
+            this.read(entityName, idOrEntity, (err, entity) => {
+                if (err) {
+                    callback(err)
+                } else {
+                    this.request( 'post', { url: url }, entity, callback)
+                }
+            })
         }
     }
 }
---
+++ ts-tests/index.spec.ts
@@ -0,0 +1,38 @@
+import { expect } from "chai"
+import config from '../config';
+import {Quickbooks} from "../src";
+
+describe("Initialization", () => {
+    it("can create Quickbooks object", () => {
+        const qb = new Quickbooks(config);
+        expect(qb).to.not.be.null;
+    })
+})
+
+describe("CRUD API", () => {
+    describe("INVOICES", () => {
+        it('can create invoice', () => {
+            const qb = new Quickbooks(config);
+            const minimalInvoice = {
+                CustomerRef: {
+                    value: "1"
+                },
+                Line: [
+                    {
+                        DetailType: "SalesItemLineDetail",
+                        Amount: 100.00,
+                        SalesItemLineDetail: {
+                            ItemRef: {
+                                name: "Services",
+                                value: "1"
+                            }
+                        }
+                    }
+                ]
+            }
+            qb.createInvoice(minimalInvoice, (err, invoice) => {
+                expect(invoice.DocNumber).to.not.be.null;
+            })
+        })
+    })
+})
