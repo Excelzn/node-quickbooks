0ddfd2517c41332a4dc5bfad65cc82d559fa5ea4
Bill Sowle <bill@timelabs.com>
2020-12-08 12:45:32 -0700

    Adding docblocks back in

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import jxon from 'jxon';
 import util from 'util';
 import axios, {Method} from 'axios';
 
+/**
+ * Configuration Object for Quickbooks API
+ */
 export interface QuickbooksConfiguration {
     consumerKey: string;
     consumerSecret: string;
@@ -19,7 +22,10 @@ export interface QuickbooksConfiguration {
 }
 
 
-
+/**
+ * Node.js client encapsulating access to the QuickBooks V3 Rest API. An instance
+ * of this class should be instantiated on behalf of each user accessing the api.
+ */
 export class Quickbooks {
     private consumerKey: string|null;
     private consumerSecret: string|null;
@@ -82,7 +88,19 @@ export class Quickbooks {
         }
     };
 
-
+    /**
+     *@constructor
+     * @param consumerKey - Application Key from QBO. For OAuth 2 apps, this is your Client ID.
+     * @param consumerSecret - Application Secret from QBO. For OAuth 2 apps, this is your Client Secret.
+     * @param token - the OAuth Access Token
+     * @param tokenSecret - the OAuth Generated User Secret. For OAuth 2 apps, this should be null as OAuth 2 does not use token secrets.
+     * @param realmId - Quickbooks Company ID, returned as a request parameter when the user is redirected to the provided callback URL following authentication
+     * @param useSandbox - Should the API use the Quickbooks Sandbox urls? See https://developer.intuit.com/v2/blog/2014/10/24/intuit-developer-now-offers-quickbooks-sandboxes
+     * @param debug - Boolean flag for HTTP Request logging.
+     * @param minorversion - sets the QBO API minor version for requests
+     * @param oauthversion - sets the OAuth version used for your application
+     * @param refreshToken - OAuth Generated Refresh Token.
+     */
     constructor(consumerKey: string|QuickbooksConfiguration, consumerSecret?: string, token?: string, tokenSecret?: string|null, realmId?: string, useSandbox?: boolean, debug?: boolean, minorversion?: string, oauthversion?: string, refreshToken?: string|null) {
         if(typeof consumerKey === 'string') {
             this.consumerKey = consumerKey;
@@ -114,6 +132,11 @@ export class Quickbooks {
         }
     }
 
+    /**
+     *  Sets the OAuth endpoints per OAuth version
+     * @param version - 1.0 for OAuth 1.0a, 2.0 for OAuth 2.0
+     * @param useSandbox - true to use the OAuth 2.0 sandbox discovery document, false (or unspecified, for backward compatibility) to use the prod discovery document.
+     */
     public setOauthVersion(version: string|number, useSandbox: boolean): void {
         version = (typeof version === 'number') ? version.toFixed(1): version;
         const discoveryUrl = useSandbox ? 'https://developer.intuit.com/.well-known/openid_sandbox_configuration/' : 'https://developer.api.intuit.com/.well-known/openid_configuration/';
@@ -124,7 +147,11 @@ export class Quickbooks {
         }, discoveryUrl);
     }
 
-    public async refreshAccessToken() {
+    /**
+     * Uses the current refresh token to obtain a new access token, using the Promise API
+     * @return Promise
+     */
+    public async refreshAccessTokenPromise() {
         const auth = (new Buffer(this.consumerKey + ':' + this.consumerSecret).toString('base64'));
         const postBody = {
             url: Quickbooks.TOKEN_URL,
@@ -146,8 +173,40 @@ export class Quickbooks {
         this.token = refreshResponse.access_token;
         return refreshResponse;
     }
-
-    public async revokeAccess(useRefresh: boolean) {
+    /**
+     * Uses the current refresh token to obtain a new access token. Wraps the Promise version in callbacks for backwards-compatibility.
+     * @param {function} callback - a function to call once the operation is complete. Takes 2 params, the first being an error object and the second being the returned data. If no error or no data, passes null for that param.
+     */
+    public refreshAccessToken(callback: (err, data) => void) {
+        this.refreshAccessTokenPromise().then(x => {
+            callback(null, x);
+        })
+            .catch(x => {
+                callback(x, null);
+            });
+    }
+    /**
+     * Use either refresh token or access token to revoke access (OAuth2).
+     * Wraps the Promise version to maintain backwards compatibility
+     *
+     * @param useRefresh - boolean - Indicates which token to use: true to use the refresh token, false to use the access token.
+     * @param {function} callback - Callback function to call with error/data results.
+     */
+    public revokeAccess(useRefresh: boolean, callback: (err, data) => void) {
+        this.revokeAccessPromise(useRefresh).then(x => {
+            callback(null, x);
+        })
+            .catch(x => {
+                callback(x, null);
+            });
+    }
+    /**
+     * Use either refresh token or access token to revoke access (OAuth2). Uses Promise API
+     *
+     * @param useRefresh - boolean - Indicates which token to use: true to use the refresh token, false to use the access token.
+     * @return Promise
+     */
+    public async revokeAccessPromise(useRefresh: boolean) {
         const auth = (new Buffer(this.consumerKey + ':' + this.consumerSecret).toString('base64'));
         const revokeToken = useRefresh ? this.refreshToken : this.token;
         const postBody = {
@@ -169,6 +228,12 @@ export class Quickbooks {
         };
         return result.data;
     }
+    /**
+     * Get user info (OAuth2).
+     *
+     * @param {function} callback - Optional Callback function to call with error/data results.
+     * @return Promise|void  Returns a Promise if the callback param is not provided, otherwise uses the callback and returns void.
+     */
     public getUserInfo(callback?: (err, data) => void): Promise<any>|void {
         if(callback != null) {
             this.request( 'get', {url: Quickbooks.USER_INFO_URL}, null).then(x => {
@@ -180,7 +245,19 @@ export class Quickbooks {
         }
         return this.request( 'get', {url: Quickbooks.USER_INFO_URL}, null);
     }
-
+    /**
+     * Batch operation to enable an application to perform multiple operations in a single request.
+     * The following batch items are supported:
+     create
+     update
+     delete
+     query
+     * The maximum number of batch items in a single request is 30.
+     *
+     * @param  {object} items - JavaScript array of batch items
+     * @param  {function} callback -Optional Callback function which is called with any error and list of BatchItemResponses.
+     * @return Promise|void  Returns a Promise if the callback param is not provided, otherwise uses the callback and returns void.
+     */
     public batch(items: Object[], callback?: (err, data) => void): Promise<any>|void {
         if(callback != null) {
             this.request( 'post', {url: '/batch'}, {BatchItemRequest: items}).then(x => {
@@ -192,6 +269,12 @@ export class Quickbooks {
         }
         return this.request( 'post', {url: '/batch'}, {BatchItemRequest: items})
     }
+
+    /**
+     *
+     * @param  {function} callback -Optional Callback function which is called with any error and list of BatchItemResponses.
+     * @return Promise|void  Returns a Promise if the callback param is not provided, otherwise uses the callback and returns void.
+     */
     public reconnect(callback?: (err, data) => void): Promise<any>|void {
         if(callback != null) {
             this.xmlRequest( Quickbooks.RECONNECT_URL ?? '', 'ReconnectResponse')
@@ -204,6 +287,11 @@ export class Quickbooks {
         }
         return this.xmlRequest( Quickbooks.RECONNECT_URL ?? '', 'ReconnectResponse');
     }
+    /**
+     *
+     * @param  {function} callback -Optional Callback function which is called with any error and list of BatchItemResponses.
+     * @return Promise|void  Returns a Promise if the callback param is not provided, otherwise uses the callback and returns void.
+     */
     public disconnect(callback?: (err, data) => void): Promise<any>|void {
         if(callback != null) {
             this.xmlRequest( Quickbooks.DISCONNECT_URL ?? '', 'PlatformResponse')
@@ -219,7 +307,14 @@ export class Quickbooks {
 
     //CRUD endpoints
     //Invoice
-    public createInvoice (invoice, callback?: (err, data) => void): Promise<any>|void {
+    /**
+     * Creates the Invoice in QuickBooks
+     *
+     * @param  {object} invoice - The unsaved invoice, to be persisted in QuickBooks
+     * @param  {function} callback - Optional Callback function which is called with any error and the persistent Invoice
+     * @return Promise|void If callback is supplied, uses the callback and returns void. Otherwise returns a promise containing the persistent Invoice.
+     */
+    public createInvoice (invoice: any, callback?: (err, data) => void): Promise<any>|void {
         if(callback != null) {
             this.create('invoice', invoice).then(x => {
                 callback(null, x);
@@ -231,8 +326,14 @@ export class Quickbooks {
         return this.create( 'invoice', invoice);
     }
 
+    /**
+     * Executes an HTTP request using Axios.
+     * @param verb - the HTTP verb to use for the request.
+     * @param options - HTTP Request options.
+     * @param entity - the Quickbooks Entity to operate on.
+     * @private
+     */
     private async request(verb: Method, options: any, entity: any): Promise<any> {
-       // const version = pjson.version;
         let url = this.endpoint + this.realmId + options.url;
         if (options.url === Quickbooks.RECONNECT_URL || options.url == Quickbooks.DISCONNECT_URL || options.url === Quickbooks.REVOKE_URL || options.url === Quickbooks.USER_INFO_URL) {
             url = options.url
@@ -258,7 +359,7 @@ export class Quickbooks {
         opts.qs.minorversion = opts.qs.minorversion || this.minorversion;
         opts.qs.format = 'json';
         //build headers
-        //opts.headers['User-Agent'] = 'node-quickbooks: version ' + version
+        opts.headers['User-Agent'] = 'node-quickbooks'
         opts.headers['Request-Id'] = uuid.v1()
         if (this.oauthversion == '2.0'){
             opts.headers['Authorization'] =  'Bearer ' + this.token
@@ -302,6 +403,13 @@ export class Quickbooks {
         }
     }
 
+    /**
+     * Executes Quickbooks XML request
+     *
+     * @param url - URL to send the request to
+     * @param rootTag - XML root tag
+     * @private
+     */
     private async xmlRequest(url: string, rootTag: string) {
        try {
            const body = await this.request( 'get', {url:url}, null);
@@ -312,6 +420,11 @@ export class Quickbooks {
            return e;
        }
     }
+
+    /**
+     * Retrieves OAuth configuration.
+     * @private
+     */
     private oauth() {
         return {
             consumer_key:    this.consumerKey,
@@ -321,13 +434,35 @@ export class Quickbooks {
         }
     }
 
+    /**
+     * Unwraps QBO response object
+     *
+     * @param data - QBO Response object to unwrap.
+     * @param entityName - Name of QBO Entity contained in the Response Object
+     * @private
+     */
     private unwrap(data, entityName): any {
         const name = this.capitalize(entityName);
         return (data || {})[name];
     }
+
+    /**
+     * Capitalizes the first letter of a string.
+     *
+     * @param s - the string to capitalize
+     * @private
+     */
     private capitalize(s: string): string {
         return s.substring(0, 1).toUpperCase() + s.substring(1)
     }
+
+    /**
+     * pluralizes an entityName
+     *
+     * @param callback
+     * @param entityName
+     * @private
+     */
     private pluralize(callback, entityName) {
         if (! callback) return function(err, data) {}
         return (err, data) => {
@@ -340,11 +475,28 @@ export class Quickbooks {
         }
     }
 
+    /**
+     * Generic Create function. Used to Create Quickbooks entities.
+     *
+     * @param entityName - Name of Entity to create, ex: Invoice
+     * @param entity - The entity to be created
+     * @return Promise - A promise containing the created entity.
+     * @private
+     */
     private async create(entityName: string, entity: any) {
         const url = '/' + entityName.toLowerCase()
         const data = await this.request( 'post', {url: url}, entity);
         return this.unwrap(data, entityName);
     }
+
+    /**
+     * Generic Read function. Used to read Quickbooks Entities.
+     *
+     * @param entityName - Name of Entity to read, ex: Invoice
+     * @param id - the string ID of the entity to read.
+     * @return Promise A promise containing the retrieved entity.
+     * @private
+     */
     private async read(entityName: string, id: string) {
         let url = '/' + entityName.toLowerCase()
         if (id) url = url + '/' + id
@@ -352,10 +504,10 @@ export class Quickbooks {
         return this.unwrap(data, entityName);
     }
     private async update(entityName: string, entity: any) {
-        if (_.isUndefined(entity.Id) ||
-            _.isEmpty(entity.Id + '') ||
-            _.isUndefined(entity.SyncToken) ||
-            _.isEmpty(entity.SyncToken + '')) {
+        if (entity.Id != null ||
+            entity.Id + '' !== '' ||
+            entity.SyncToken != null ||
+            entity.SyncToken + '' !== '') {
             if (entityName !== 'exchangerate') {
                 throw new Error(entityName + ' must contain Id and SyncToken fields: ' +
                     util.inspect(entity, {showHidden: false, depth: null}))
@@ -375,14 +527,14 @@ export class Quickbooks {
     }
     private async delete(entityName: string, idOrEntity: string|any) {
         const url = '/' + entityName.toLowerCase() + '?operation=delete'
-        if (_.isObject(idOrEntity)) {
+        if (typeof idOrEntity !== 'string') {
             return await this.request( 'post', {url: url}, idOrEntity);
         } else {
             const entity = await this.read( entityName, idOrEntity);
             return await this.request('post', {url: url}, entity);
         }
     }
-    private async void(entityName: string, idOrEntity: string|any, callback) {
+    private async void(entityName: string, idOrEntity: string|any) {
         const url = '/' + entityName.toLowerCase() + '?operation=void'
         if (_.isObject(idOrEntity)) {
             return this.request( 'post', { url: url }, idOrEntity)
